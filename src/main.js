37cb09bac16a838b6f3e85275680ce498f836e71
Samuel CC <cc20019@ues.edu.sv>
2023-05-19 18:30:39 -0600

    ok

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 //url del backend recuerda quitar cualquier pleca del final / quitarla
-axios.defaults.baseURL= process.env.BACKEND || "http://backend2.samuelcabezas.me/"
+axios.defaults.baseURL= process.env.BACKEND || "http://backend2.samuelcabezas.me"
 //axios.defaults.baseURL= process.env.BACKEND || "http://backend.io"
 //axios.defaults.baseURL= process.env.BACKEND || "https://ejercioejemploues.herokuapp.com/"
 
